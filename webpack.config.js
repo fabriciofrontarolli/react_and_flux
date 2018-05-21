80bb79f3e18afd02bba67ea0f1fb31ab1fc3ddab
Fabricio Frontarolli <fabricio.frontarolli@gmail.com>
2018-05-21 00:00:39 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,27 @@
+const path = require('path');
 module.exports = {
-	entry: "./client.js",
-	output: {
-		path: "./deploy",
-		filename: "app.js"
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: "babel"
-			}
-		]
-	},
-	watch: true
-};
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }, {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      }
+    ]
+  }
+}
